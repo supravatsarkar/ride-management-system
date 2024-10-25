d28f23d097a1413e11bbea4f9c03d8c893570436
Supravat Sarkar <supravatsarkar18@gmail.com>
2024-10-25 15:30:02 +0530

    refactor files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const config = require("./config/index.js");
-const logger = require("./helper/logger.js");
+const config = require("./src/config/index.js");
+const logger = require("./src/helper/logger.js");
 const app = require("./src/app.js");
 
 app.listen(config.port, () => {
